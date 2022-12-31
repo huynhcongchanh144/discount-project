98f9e90c2d40fcf9f24ac9c93c4794b6b3bd6a4d
ChanhHuynh <chanh.huynh@teamscal.com>
2022-12-31 10:42:50 +0700

    build magazin page

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,42 @@ let collapseBtn = document.querySelectorAll('.collapse-btn')
 collapseBtn.forEach(e => {
     e.addEventListener('click', () => {
         let target = e.getAttribute("target");
-        console.log(target)
+        let collapse = document.getElementById(target)
+        if(collapse.classList.contains('collapse')) {
+            collapse.classList.remove('collapse')
+            e.classList.remove('btn-view-up')
+            e.classList.add('btn-view-down')
+        } else {
+            collapse.classList.add('collapse')
+            e.classList.add('btn-view-up')
+            e.classList.remove('btn-view-down')
+        }
     })
 })
+
+let moreDetails = document.querySelectorAll('.more-details')
+
+moreDetails.forEach(e => {
+    let tabs = e.querySelectorAll('.tab')
+    tabs.forEach(tab => {
+        tab.addEventListener('click',() => {
+            let target = tab.getAttribute('target')
+            let content = document.getElementById(target)
+            content.classList.remove('collapse')
+            tab.classList.add('active')
+            removeActive(e, target)
+        })
+    })
+})
+
+function removeActive(e, target) {
+    let tabActive = e.querySelectorAll('.tab')
+    tabActive.forEach(tab => {
+        let subTarget = tab.getAttribute('target')
+        let content = document.getElementById(subTarget)
+        if(!tab.getAttribute('target').includes(target)) {
+            tab.classList.remove('active')
+            content.classList.add('collapse')
+        }
+    })
+}
